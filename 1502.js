cb6fd55ab19cfd62e764abc6561b25543daedc87
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-06-06 21:19:49 -0400

    Update 1502.js

--- 1502.js
+++ 1502.js
@@ -2,16 +2,19 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var canMakeArithmeticProgression = function (arr) {
-  let sorted = arr.sort();
-  let diff = sorted[1] - sorted[0]
+let canMakeArithmeticProgression = function (arr) {
+  let sorted = arr.sort(function (a, b) { return a - b });
+  let diff = sorted[1] - sorted[0];
   // console.log(sorted)
+  if (arr.length == 2) {
+    return true
+  }
   for (i = 2; i < sorted.length; i++) {
     let apples = sorted[i] - sorted[i - 1]
     if (apples !== diff) {
       return false
-    } else {
-      return true
+
     }
   }
+  return true
 };       
